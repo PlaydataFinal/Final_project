7b40e04f0f44a6d132403ae7c509bcd40e7d8ed9
joowan <joowan119@gmail.com>
2024-01-11 17:24:35 +0900

    succes

--- basic/src/main/java/com/example/basic/service/WebService.java
+++
@@ -1,9 +0,0 @@
-package com.example.basic.service;
-
-import com.example.basic.database.entity.WebEntity;
-
-public interface WebService {
-    public void joinUserDto(WebEntity entity);
-
-    public void updateIsLoginByName(String name, Boolean isLogin);
-}
---
+++ travel/pybo/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
+
+def index(request):
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
