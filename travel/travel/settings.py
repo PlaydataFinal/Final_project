eb85f2cd4b80dac3def75b25f64049f50048875c
krkim <kgs08101220@naver.com>
2024-01-23 09:46:46 +0900

    clear dummy

--- travel/travel/settings.py
+++ travel/travel/settings.py
@@ -121,13 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-<<<<<<< HEAD
-=======
-
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
->>>>>>> joowan
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
