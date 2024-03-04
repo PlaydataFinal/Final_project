924c837fb150af4548db972249666be2ea63ef7f
joowan119 <joowan119@gmail.com>
2024-03-04 03:19:28 +0000

    ocr

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -30,7 +30,8 @@ urlpatterns = [
     path('result/', views.result, name='result'),
     path('main/', views.main, name='main'),
     path('common/', include('common.urls')),
-    path('kakaoapi/', include('kakaoapi.urls'))
+    path('kakaoapi/', include('kakaoapi.urls')),
+    path('ocr/', include('ocr.urls')),
 ]
 
 if settings.DEBUG:
