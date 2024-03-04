a62533ea6dfa7aeda2f4036d93cf1beb76b94c98
krkim <kgs08101220@naver.com>
2024-03-04 14:26:19 +0900

    240304_Afternoon

--- django/mysite/mysite/utils.py
+++ django/mysite/mysite/utils.py
@@ -48,17 +48,17 @@ def recommend_places(user_input):
     # answer 까지는 얼추 맞음
     
     #names 부터 수정 필요
-    names = answer.sort_values('similarity', ascending=False)['PrimaryKey'].tolist()[:5]
-    result = df[df['PrimaryKey'].isin(names)].set_index('PrimaryKey').loc[names].reset_index()
+    names = answer.sort_values('similarity', ascending=False)['Name'].tolist()[:5]
+    result = df[df['Name'].isin(names)].set_index('Name').loc[names].reset_index()
 
     # 추천 장소 이름 출력
 # 추천 장소 이름 출력
-    recommended_names = ", ".join(result['PrimaryKey'])
+    recommended_names = ", ".join(result['Name'])
     output_text = f"추천하는 관광지는 {recommended_names}입니다.\n"
 
     # 각 장소에 대한 정보 출력
     for _, row in result.iterrows():
-        text = f"\n{row['PrimaryKey']}에 대한 정보입니다:\n"
+        text = f"\n{row['Name']}에 대한 정보입니다:\n"
         text += f"주소는 {row['Address']}에 있습니다.\n"
         text += f"전화번호는 {row['Tel']}입니다.\n"
         text += f"상세 정보는 {row['detail_box_elements']}\n"
