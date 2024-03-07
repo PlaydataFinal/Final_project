5eca27321be2559099af081b71da8fcb010945cb
joowan119 <joowan119@gmail.com>
2024-03-07 08:47:33 +0000

    kakaomap api 80%success

--- django/mysite/kakaoapi/views.py
+++ django/mysite/kakaoapi/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import tour_kakao
 from .serializers import TourKakaoSerializer
+from rest_framework.pagination import PageNumberPagination
 # Create your views here.
 from django.http import HttpResponse
 
@@ -9,11 +10,14 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'kakaoapi/kakao.html')# kakaoapi/views.py
 
-
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 15  # 한 페이지당 아이템 수
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class TourKakaoList(generics.ListAPIView):
     serializer_class = TourKakaoSerializer
-
+    pagination_class = CustomPageNumberPagination 
     def get_queryset(self):
         keyword = self.request.query_params.get('keyword', '')
         return tour_kakao.objects.filter(Name__icontains=keyword)
