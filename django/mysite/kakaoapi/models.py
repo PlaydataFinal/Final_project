ca6b18e8a2962c9ffcabcb69e7fd84982ee9f972
krkim <kgs08101220@naver.com>
2024-03-11 17:50:08 +0900

    modify models

--- django/mysite/kakaoapi/models.py
+++ django/mysite/kakaoapi/models.py
@@ -21,13 +21,13 @@ class tour_kakao(models.Model):
     
 # 240311 추가내용
 class tour_comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_tour_comment')
     tour = models.ForeignKey(tour_kakao, on_delete=models.CASCADE)
     comment = models.TextField(db_column='Comment')
     comment_like = models.IntegerField(db_column='Comment_Like', default=0)
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-
+    like = models.ManyToManyField(User, related_name='like_tour_comment')
     
     def __str__(self):
         return self.Name
