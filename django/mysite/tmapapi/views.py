6971e4a544d01b14cd12757bd079c74953671bc4
threee <tmfgml456@gmail.com>
2024-03-13 18:28:05 +0900

    'steeljoowan'

--- django/mysite/tmapapi/views.py
+++ django/mysite/tmapapi/views.py
@@ -1,3 +1,4 @@
+#tmapapi/views.py
 from django.shortcuts import render
 from rest_framework import generics
 from .models import tour_tmap
@@ -5,6 +6,8 @@ from .serializers import TourTmapSerializer
 
 # Create your views here.
 from django.http import HttpResponse
+from rest_framework.response import Response
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -15,5 +18,27 @@ class TourTmapList(generics.ListAPIView):
     serializer_class = TourTmapSerializer
 
     def get_queryset(self):
-        keyword = self.request.query_params.get('keyword', '')
-        return tour_tmap.objects.filter(Name__icontains=keyword)
+        departure = self.request.query_params.get('departure', '')
+        destination = self.request.query_params.get('destination', '')
+        print(f"keyword --> {departure}")
+        print(f"keyword --> {destination}")
+        print(f"keyword --> {self.request}")
+        queryset = tour_tmap.objects.all()
+
+        if departure or destination:
+            queryset = queryset.filter(Q(Name__icontains=departure) | Q(Name__icontains=destination))
+            print(queryset)
+
+        return queryset
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+
+    def tour_detail(request, tour_id):
+        tour_list = tour_tmap.objects.get(id=tour_id) 
+        tour_all = tour_tmap.objects.all()
+        content = {"tour" : tour_list, "tour_all" : tour_all}
+        return render(request, "tmap.html", content)
